b5527cee90af42acebd34ae904f885e83688b07f
James Hines <jameshines10@gmail.com>
2015-06-08 22:17:41 -0400

    Adds empty files used in the book for testing examples

---
+++ src/main/java/iloveyouboss/Answer.java
@@ -0,0 +1,33 @@
+package iloveyouboss;
+
+public class Answer {
+	private int i;
+	private Question question;
+
+	public Answer(Question question, int i) {
+		this.question = question;
+		this.i = i;
+	}
+
+	public String getQuestionTest() {
+		return question.getText();
+	}
+
+	@Override
+	public String toString() {
+		return String.format("%s %s", question.getText(), question.getAnswerChoice(i));
+	}
+
+	public boolean match(int expected) {
+		return question.match(expected, i);
+	}
+
+	public boolean match(Answer otherAnswer) {
+		return question.match(i, otherAnswer.i);
+	}
+
+	public Question getCharacteristic() {
+		return question;
+	}
+
+}
